7f2e2efe780368ad44f1cf6035322216334cec87
karolrojas07 <karol.rojasb.kommit@gmail.com>
2022-03-26 12:53:56 -0500

    Fix env problem

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express');
+require("dotenv").config();
 const { default: mongoose } = require('mongoose');
-const app = express();
-const port = process.env.port || 3000;
+const APP = express();
+const PORT = process.env.PORT || 3000;
 const routerApi = require('./src/routes')
 
-require("dotenv").config();
 
-app.listen(port, () => console.log('Listen port', port));
-app.use(express.json());
+
+APP.listen(PORT, () => console.log('Listen PORT', PORT));
+APP.use(express.json());
 
 mongoose
     .connect(process.env.MONGODB_CONNECTION_STRING)
     .then(() => console.log("Succes connection with mongo"))
     .catch(() => console.error("Connection could not be established"));
 
-routerApi(app);
+routerApi(APP);
